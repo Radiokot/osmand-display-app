dacff38e74a3b2bc789967d3b105076b91bcd480
Oleg Koretsky <oleg@radiokot.com.ua>
2023-01-13 23:18:45 +0200

    Add a stub model for the imported track

---
+++ app/src/main/java/ua/com/radiokot/osmanddisplay/features/map/track/model/ImportedTrack.kt
@@ -0,0 +1,18 @@
+package ua.com.radiokot.osmanddisplay.features.map.track.model
+
+import java.io.File
+import java.time.Instant
+
+class ImportedTrack() {
+    val name: String
+        get() = TODO()
+
+    val importedAt: Instant
+        get() = TODO()
+
+    val previewImageFile: File
+        get() = TODO()
+
+    val geoJsonFile: File
+        get() = TODO()
+}
